33058a9b6cefddec6657ef0b567875bd99bcd8e1
roman-myspace <roman.my.workspace@gmail.com>
2023-12-04 14:27:08 +0500

    style changes

--- components/challenges/qr-code-component/qr-card.tsx
+++ components/challenges/qr-code-component/qr-card.tsx
@@ -7,8 +7,8 @@ export default function QRCard() {
                 <img src="./images/image-qr-code.png" alt='QR-Code Image' className="card__image" />
             </figure>
             <div className="card__body">
-                <h1 className="">Improve your front-end skills by building projects</h1>
-                <p>
+                <h1 className="text-xl font-semibold">Improve your front-end skills by building projects</h1>
+                <p className='mt-2'>
                     Scan the QR code to visit Frontend Mentor and take your coding
                     skills to the next level
                 </p>
