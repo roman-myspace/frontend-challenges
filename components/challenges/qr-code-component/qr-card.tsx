d740a79489c394f67ad30878eb808c6769dbedf1
roman-myspace <roman.my.workspace@gmail.com>
2023-12-04 14:24:05 +0500

    level one heading added

--- components/challenges/qr-code-component/qr-card.tsx
+++ components/challenges/qr-code-component/qr-card.tsx
@@ -7,7 +7,7 @@ export default function QRCard() {
                 <img src="./images/image-qr-code.png" alt='QR-Code Image' className="card__image" />
             </figure>
             <div className="card__body">
-                <h6 className="">Improve your front-end skills by building projects</h6>
+                <h1 className="">Improve your front-end skills by building projects</h1>
                 <p>
                     Scan the QR code to visit Frontend Mentor and take your coding
                     skills to the next level
